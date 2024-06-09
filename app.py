2bc152499e49ce3f4e0eabdfdc0a4a91e047df2d
thoahlane10111 <thoahlane10111>
2024-06-09 08:31:50 +0000

    latest

--- app.py
+++ app.py
@@ -1,19 +1,2 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-# Define routes
-@app.route('/')
-def index():
-    # Render the index.html template
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    # Render the about.html template
-    return render_template('about.html')
-
-# Run the Flask app
-if __name__ == '__main__':
-    app.run(debug=True)
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
